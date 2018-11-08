af02c446d9fc68dd5bee6644236c86bdbfed4f47
jatin-dev <39021650+jatin-dev@users.noreply.github.com>
2018-11-08 23:35:38 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VSCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start the server  
+            //TcpHelper.StartServer(5678);  
+            //TcpHelper.Listen(); // Start listening.
+
+
+           // AsynchronousSocketListener.StartListening(); 
+
+           AsynchronousClient.StartClient();
+            return;
+        }
+    }
+}
